84bcaf20994c1c85bc22549e8d4636bab0f8722e
Eduarda97-fa <eduardaf9939@gmail.com>
2024-06-11 09:06:56 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,64 @@
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    circle(xBolinha, yBolinha, diametro);
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+if (xBolinha > width || xBolinha < 0) {
+        velocidadeXBolinha *= -1;
+    }if (yBolinha > height || yBolinha < 0) {
+        velocidadeYBolinha *= -1;
+}
+
+function draw() {
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+}
+
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro)
+}
+
+function movimentaBolinha() {
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda() {
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
+        velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
+        velocidadeYBolinha *= -1;
+    }
+}
+//variáveis da bolinha
+letxBolinha = 300;
+letyBolinha = 200;
+letdiametro = 15;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+letvelocidadeXBolinha = 6;
+letvelocidadeYBolinha = 6;
+function draw() {
+  background(0); //1 - Desenha o background 
+  mostraBolinha(); // 2 - Desenha a bolinha
+  movimentaBolinha(); // 3 - Movimenta a Bolinha
+  verificaColisaoBorda(); // 4 - Verifica Colisão da bolinha
+ 
+ // 5- Volta para o início da função draw()
+}
+  
